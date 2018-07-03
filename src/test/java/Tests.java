c6aa6b465ae53983fc9055df2344bfd9097f24a0
Maks <mixer.lviv.ua@gmail.com>
2018-07-03 16:44:40 +0300

    Added self handling

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,22 +1,21 @@
-import com.microsoft.auth.AuthContext;
 import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.auth.AuthContext;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Tests {
     @Test
-    public void text() throws MalformedURLException, ExecutionException, InterruptedException {
+    public void getOneToken() throws MalformedURLException, ExecutionException, InterruptedException {
         String authURL = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
         String resource = "https://graph.microsoft.com";
         ExecutorService service = Executors.newFixedThreadPool(1);
 
         AuthContext context = new AuthContext(authURL, false, service);
-        Future<AuthenticationResult> future = context.acquireToken(
+        AuthenticationResult result = context.acquireToken(
                 resource,
                 "<client_id>",
                 "<client_secret>",
@@ -25,10 +24,29 @@ public class Tests {
                 null
         );
 
-        AuthenticationResult result = future.get();
         System.out.println("Access Token - " + result.getAccessToken());
         System.out.println("Refresh Token - " + result.getRefreshToken());
         System.out.println("ID Token - " + result.getIdToken());
         service.shutdown();
+    }
+
+    @Test
+    public void getTokenHandler() throws MalformedURLException, ExecutionException, InterruptedException {
+        AuthContext context = new AuthContext(
+                "<client_id>",
+                "<client_secret>",
+                "<user_login>",
+                "<user_password>",
+                null);
+        AuthenticationResult result = context.acquireToken();
+        System.out.println("Access Token - " + result.getAccessToken());
+        System.out.println("Refresh Token - " + result.getRefreshToken());
+        System.out.println("ID Token - " + result.getIdToken());
+
+        result = context.refreshToken();
+        System.out.println("Access Token - " + result.getAccessToken());
+        System.out.println("Refresh Token - " + result.getRefreshToken());
+        System.out.println("ID Token - " + result.getIdToken());
+        context.shutdownService();
     }
 }
